d509828928c117e219abfbb32919da75a8c7be5a
Nicolas <nick.nogueira02@gmail.com>
2025-05-04 16:28:28 -0300

    closes #2 - header criado

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   standalone: false,
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+title = 'Car Systems 1.0';
 
 }
